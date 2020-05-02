f930667f66bc596c47e93036ce469b03dd074879
Kobi32 <lisowski.lukasz89@gmail.com>
2020-05-02 23:11:14 +0200

    server

--- garageapp/settings.py
+++ garageapp/settings.py
@@ -19,6 +19,7 @@ REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
 
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static'),
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Quick-start development settings - unsuitable for production
